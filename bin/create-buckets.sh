d8827ebe2847e97c53003ec744e6d135499e97eb
Daniel Macedo <admacedo@gmail.com>
2017-09-07 12:42:47 +0100

    Hide expected error on creating bucket deps

--- bin/create-buckets.sh
+++ bin/create-buckets.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 echo "Create Foundation S3 bucket: awsrig.${PROJECT}.${NAME_SUFFIX}.foundation"
-aws s3api head-bucket --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.foundation" --region "${REGION}" ||
+aws s3api head-bucket --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.foundation" --region "${REGION}" 2> /dev/null ||
   aws s3 mb s3://awsrig.${PROJECT}.${NAME_SUFFIX}.foundation  --region "${REGION}" # Foundation configs
 aws s3api put-bucket-versioning --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.foundation" --versioning-configuration Status=Enabled --region "${REGION}"
 
 echo "Create InfraDev S3 bucket: awsrig.${PROJECT}.${NAME_SUFFIX}.infradev"
-aws s3api head-bucket --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.infradev" --region "${REGION}" ||
+aws s3api head-bucket --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.infradev" --region "${REGION}" 2> /dev/null ||
   aws s3 mb s3://awsrig.${PROJECT}.${NAME_SUFFIX}.infradev --region "${REGION}" # Storage for InfraDev
 aws s3api put-bucket-versioning --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.infradev" --versioning-configuration Status=Enabled --region "${REGION}"
 
 echo "Create Build Artifacts S3 bucket: awsrig.${PROJECT}.${NAME_SUFFIX}.build"
-aws s3api head-bucket --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.build" --region "${REGION}" ||
+aws s3api head-bucket --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.build" --region "${REGION}" 2> /dev/null ||
   aws s3 mb s3://awsrig.${PROJECT}.${NAME_SUFFIX}.build --region "${REGION}" # Build artifacts, etc
 aws s3api put-bucket-versioning --bucket "awsrig.${PROJECT}.${NAME_SUFFIX}.build" --versioning-configuration Status=Enabled --region "${REGION}"
