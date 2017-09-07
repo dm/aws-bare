4a3b49051517b354a1bf53935ce038d6883ba71d
Daniel Macedo <admacedo@gmail.com>
2017-09-07 14:51:16 +0100

    Fix config names

--- bin/build-configs.sh
+++ bin/build-configs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd config/
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' config-dev.json.tpl > config-dev.json
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' config-stg.json.tpl > config-stg.json
-perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' config-prd.json.tpl > config-prd.json
+perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' config.dev.json.tpl > config.dev.json
+perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' config.stg.json.tpl > config.stg.json
+perl -p -e 's/\$\{([^}]+)\}/defined $ENV{$1} ? $ENV{$1} : $&/eg; s/\$\{([^}]+)\}//eg' config.prd.json.tpl > config.prd.json
 zip "${PROJECT}-${NAME_SUFFIX}-foundation.zip" config*.json
